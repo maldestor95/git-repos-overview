221a0a4cb13dd2afa061fa4afe84d7c7e80501c7
maldestor95 <maldestor95@gmail.com>
2024-06-09 06:19:57 +0200

    refactor all code

--- lib/gitstatus.ts
+++ lib/gitstatus.ts
@@ -2,33 +2,49 @@ import childprocess from "node:child_process"
 import util from 'util'
 const execPromise = util.promisify(childprocess.exec);
 
-
-
 type gitStatusType = {
     path: string
     status: string
 }
+type gitStatusResultType = Awaited<ReturnType<typeof gitStatus>>
+
 const gitFecthUno = 'git fetch && git status -uno'
 const gitStatusCmd = 'git status'
 const git_command = gitStatusCmd
+
 const notARepo = `not a git repository`
-export default async (path: string) => {
+
+/** 
+ * @description
+ * return the Git Status of a folder
+ * @param 
+ * path
+ * @returns
+ * Object with {{key:initial_path, status:git_execution_results}}
+ * 
+ * the git execution results are static string reflecting:
+ * upTodateStatus
+ * branch_behind
+ * branch_ahead
+ * untracked_files
+ * HEAD_detached
+ * 
+ */
+const gitStatus = async (path: string) => {
     const { stdout, stderr } = <Record<string, string>>await execPromise(git_command, { cwd: path }).catch(err => err)
     if (stderr) {
         if (stderr.includes(notARepo))
             return { path: path, status: "not a git repository" }
         return { path: path, status: `${path}=> ${stderr}` }
-        // console.log(`stderr: ${path}=> ${stderr}`)
     }
-
     return {
-        path: path, status: getStatus(stdout, path)
+        path: path, status: extractStatusFromGitResponse(stdout, path)
     }
 }
+export default gitStatus
+export { gitStatusType, displayNotAGitRepo, displaytGitSync, displayNotGitSync, gitStatusResultType }
 
-export { gitStatusType, }
-
-const getStatus = (stdout: string, path: string) => {
+const extractStatusFromGitResponse = (stdout: string, path: string) => {
     const Changes_not_staged = `Changes not staged for commit`
     const upTodateStatus = `branch is up to date`
     const branch_behind = `branch is behind`
@@ -45,4 +61,20 @@ const getStatus = (stdout: string, path: string) => {
     if (stdout.includes(untracked_files)) return untracked_files
     if (stdout.includes(HEAD_detached)) return HEAD_detached
     return `${path}==>${stdout}`
+}
+
+const displayNotAGitRepo = (gitFolderStatus: gitStatusResultType[]) => {
+    const noRepo = gitFolderStatus.filter(ff => ff.status == 'not a git repository')
+    console.error(`\n Folders with no repos`)
+    noRepo.forEach(ff => console.log(ff.path))
+}
+const displaytGitSync = (gitFolderStatus: gitStatusResultType[]) => {
+    const noRepo = gitFolderStatus.filter(ff => ff.status.includes('branch is up to date'))
+    console.log(`\n Branch is up to date`)
+    noRepo.forEach(ff => console.log(ff.path))
+}
+const displayNotGitSync = (gitFolderStatus: gitStatusResultType[]) => {
+    const noRepo = gitFolderStatus.filter(ff => (!ff.status.includes('not a git repository') && !ff.status.includes('branch is up to date')))
+    console.log(`\n Folders with  repos not syncronized`)
+    noRepo.forEach(ff => console.log(ff.path))
 }
